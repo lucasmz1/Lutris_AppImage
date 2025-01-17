24c4bd9a03997b4aac91740bc48ade6a1fe15106
Lucas Machado Zainote <95252125+lucasmz1@users.noreply.github.com>
2025-01-17 16:17:38 -0300

    Update build_lutris.sh

--- build_lutris.sh
+++ build_lutris.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 sudo apt install -y wget binutils coreutils desktop-file-utils
-wget -c -q "https://github.com/AppImageCrafters/appimage-builder/releases/download/v1.1.0/appimage-builder-1.1.0-x86_64.AppImage" -o appimage-builder ; chmod +x appimage-builder
+wget -c -q "https://github.com/AppImageCrafters/appimage-builder/releases/download/v1.1.0/appimage-builder-1.1.0-x86_64.AppImage" -O appimage-builder ; chmod +x appimage-builder
 ./appimage-builder --recipe lutris.yml --skip-appimage
 cp -f sitecustomize.py ./AppDir/etc/python3.*/
 cp -f loaders.cache ./AppDir/usr/lib/x86_64-linux-gnu/gdk-pixbuf-2.0/2.10.0/
-wget -c -q "https://github.com/AppImage/appimagetool/releases/download/continuous/appimagetool-x86_64.AppImage" -o appimagetool ; chmod +x appimagetool
-./appimagetool -n ./AppDir/
+wget -c -q "https://github.com/AppImage/appimagetool/releases/download/continuous/appimagetool-x86_64.AppImage" -O appimagetool ; chmod +x appimagetool
+ARCH=x86_64 ./appimagetool -n ./AppDir/
