ed00ce6f2e307d67f1d169c1c7794be3031b8691
Lucas Machado Zainote <95252125+lucasmz1@users.noreply.github.com>
2025-01-26 11:56:30 -0300

    Update build_lutris.sh

--- build_lutris.sh
+++ build_lutris.sh
@@ -6,7 +6,7 @@ wget -c -q "https://github.com/AppImageCrafters/appimage-builder/releases/downlo
 ./appimage-builder --recipe lutris.yml --skip-appimage --skip-tests
 cp -f sitecustomize.py ./AppDir/etc/python3.*/
 cp -f loaders.cache ./AppDir/usr/lib/x86_64-linux-gnu/gdk-pixbuf-2.0/2.10.0/
-cp -f net.lutris.Lutris.desktop ./AppDir/
+cp -f lutris.desktop ./AppDir/
 cp -f lutris.png ./AppDir/
 wget -c -q "https://github.com/AppImage/appimagetool/releases/download/continuous/appimagetool-x86_64.AppImage" -O appimagetool ; chmod +x appimagetool
 ARCH=x86_64 ./appimagetool -n ./AppDir/
