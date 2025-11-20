07b08118a0320dce538fa5ca0d4a53dccfa73488
iVAN <88724353+ivan-hc@users.noreply.github.com>
2025-11-21 00:51:21 +0100

    Update the JuNest-based script template (#6)
    * Get Archimage version from the builder
    
    * Move AUR handling to the Archimage builder

--- lutris-junest.sh
+++ lutris-junest.sh
@@ -72,9 +72,6 @@ _enable_multilib
 #	INSTALL PROGRAMS USING YAY
 ##########################################################################################################################################################
 
-_JUNEST_CMD -- yay -Syy
-#_JUNEST_CMD -- gpg --keyserver keyserver.ubuntu.com --recv-key C01E1CAD5EA2C4F0B8E3571504C367C218ADD4FF # UNCOMMENT IF YOU USE THE AUR
-
 [ -f ../archimage-builder.sh ] && source ../archimage-builder.sh install "$@"
 
 cd ..
@@ -137,5 +134,4 @@ _appimagetool() {
 	fi
 }
 
-ARCH=x86_64 _appimagetool -u "$UPINFO" \
-	AppDir "$APPNAME"_"$VERSION"-archimage5.0-x86_64.AppImage
+ARCH=x86_64 _appimagetool -u "$UPINFO" AppDir "$APPNAME"_"$VERSION"-"$ARCHIMAGE_VERSION"-x86_64.AppImage
